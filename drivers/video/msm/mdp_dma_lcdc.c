485c71b916d7d611cd82fa7a4921a97149340028
Jeevan Shriram <jshriram@codeaurora.org>
2011-12-19 13:43:44 +0530

    msm_fb: display: Add mutex during display turn off
    There is a possibility that suspend operation trigger during
    a display update. Add mutex to serialize suspend and display update
    operations
    
    Change-Id: I52dea1bd718e8c54d894ae3830e86105f951efab
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp_dma_lcdc.c

--- drivers/video/msm/mdp_dma_lcdc.c
+++ drivers/video/msm/mdp_dma_lcdc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2009, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2008-2009, 2012 Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -352,6 +352,7 @@ int mdp_lcdc_off(struct platform_device *pdev)
 #ifdef CONFIG_HUAWEI_KERNEL
 	ret = panel_next_off(pdev);
 #endif
+	down(&mfd->dma->mutex);
 	/* MDP cmd block enable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 	MDP_OUTP(MDP_BASE + timer_base, 0);
@@ -361,6 +362,8 @@ int mdp_lcdc_off(struct platform_device *pdev)
 #ifndef CONFIG_HUAWEI_KERNEL
 	ret = panel_next_off(pdev);
 #endif
+	up(&mfd->dma->mutex);
+
 	/* delay to make sure the last frame finishes */
 	msleep(16);
 
@@ -398,6 +401,7 @@ void mdp_lcdc_update(struct msm_fb_data_type *mfd)
 	if (!mfd->panel_power_on)
 		return;
 
+	down(&mfd->dma->mutex);
 	/* no need to power on cmd block since it's lcdc mode */
 	bpp = fbi->var.bits_per_pixel / 8;
 	buf = (uint8 *) fbi->fix.smem_start;
@@ -434,4 +438,5 @@ void mdp_lcdc_update(struct msm_fb_data_type *mfd)
 	spin_unlock_irqrestore(&mdp_spin_lock, flag);
 	wait_for_completion_killable(&mfd->dma->comp);
 	mdp_disable_irq(irq_block);
+	up(&mfd->dma->mutex);
 }
