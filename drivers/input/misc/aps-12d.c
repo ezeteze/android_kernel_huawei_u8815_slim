7d44619fb0084d2c3c9e3e4984f943a13267ea5b
qiwu <ivan198@gmail.com>
2012-08-11 21:19:20 +0800

    多次初始化APS-12D，以便APS-12D正确初始化［距感,光感］

--- drivers/input/misc/aps-12d.c
+++ drivers/input/misc/aps-12d.c
@@ -705,14 +705,17 @@ static int aps_12d_probe(
 
 	/* < DTS2010081803338 zhangtao 20100818 begin */
 	/* make the rang smaller can make the ir changge bigger */
-	ret = aps_i2c_reg_write(aps, APS_12D_REG_CMD2, \
-/* < DTS2010102103994 zhangtao 20101112 begin */
+	
+	ret=-1;
+	for(i=0;i<10 && ret!=0;i++) {
+		ret = aps_i2c_reg_write(aps, APS_12D_REG_CMD2, \
 	                         (uint8_t)(APS_12D_IRDR_SEL_50MA << 6 | \
 	                                   APS_12D_FREQ_SEL_DC << 4 | \
 	                                   APS_12D_RES_SEL_12 << 2 | \
 	                                   APS_12D_RANGE_SEL_ALS_1000));
-/* DTS2010102103994 zhangtao 20101112 end > */
-	/* DTS2010081803338 zhangtao 20100818 end > */
+		printk("aps_12d_probe try %d\n",i);
+		mdelay(5);
+	}
 	if(ret < 0)
 	{
 		goto err_detect_failed;
